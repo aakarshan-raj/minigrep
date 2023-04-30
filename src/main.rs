0d90947ad6349ed5b1e44a00cd53e764d12322d9
aakarshan-raj <aakarshan7raj@gmail.com>
2023-04-30 23:01:48 +0530

    useless code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::{self,Read};
+
+
 fn main() {
-    println!("Hello, world!");
+    let data = read_the_file("poemdsf.txt");
+    println!("{data}");
+}
+
+fn read_the_file(file_name:&str)->String{
+
+ let mut x = match File::open(file_name){
+      Ok(yes)=>yes,
+      Err(e)=>panic!("{e}"),
+   };
+ let mut username = String::new();
+ let data = x.read_to_string(&mut username);
+ return username;
+
 }
