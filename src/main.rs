40e2147de5d007fe39dae4a943b9425114ae60f1
aakarshan-raj <aakarshan7raj@gmail.com>
2023-04-30 23:03:14 +0530

    clean code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-use std::fs::File;
-use std::io::{self,Read};
-
 
 fn main() {
-    let data = read_the_file("poemdsf.txt");
-    println!("{data}");
-}
-
-fn read_the_file(file_name:&str)->String{
-
- let mut x = match File::open(file_name){
-      Ok(yes)=>yes,
-      Err(e)=>panic!("{e}"),
-   };
- let mut username = String::new();
- let data = x.read_to_string(&mut username);
- return username;
+  
 
 }
+
+
