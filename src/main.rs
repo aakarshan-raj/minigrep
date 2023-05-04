544b4bad889e67b5ed11def531aa208fed195cc4
aakarshan-raj <aakarshan7raj@gmail.com>
2023-05-04 21:57:36 +0530

    parsing logic separated

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
 use std::env;
+use std::process;
+
+fn main(){
+
+let vector:Vec<String> = env::args().collect();
+let config = Config::build(&vector).unwrap_or_else(|err|{
+
+        println!("Error cause:{err}");
+        process::exit(1);
+
+ });
+
+println!("search for:{} in: {}",config.query,config.file_path);
+
+}
+
 
 struct Config{
 
-  query:String,
-  file_path:String,
+query:String,
+file_path:String,
 
 }
 
-fn main() {
-  let v:Vec<String> = env::args().collect();
-  let config = parse_config(&v); 
+impl Config{
 
-}
+   fn build(vector:&Vec<String>)->Result<Config,&'static str>{
 
-fn parse_config(v:&Vec<String>)->Config{
+       if vector.len() < 3 {
+         return Err("Argument error");
+       }
+       let query = vector[1].clone();
+       let file_path = vector[2].clone();
+       Ok(Config{query,file_path})
 
-  let a = v[1].clone();
-  let b = v[2].clone();
-  
-  Config{query:a,file_path:b}
+   }
 
 }
