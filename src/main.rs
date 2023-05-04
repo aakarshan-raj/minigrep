82ba09995040938e1372ea39cc479d872e2ea0ad
aakarshan-raj <aakarshan7raj@gmail.com>
2023-05-04 21:15:18 +0530

    starting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::env;
 
-fn main() {
-  
+struct Config{
+
+  query:String,
+  file_path:String,
 
 }
 
+fn main() {
+  let v:Vec<String> = env::args().collect();
+  let config = parse_config(&v); 
 
+}
+
+fn parse_config(v:&Vec<String>)->Config{
+
+  let a = v[1].clone();
+  let b = v[2].clone();
+  
+  Config{query:a,file_path:b}
+
+}
